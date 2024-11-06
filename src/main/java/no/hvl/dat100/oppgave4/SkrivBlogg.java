5f331de668104019ac3382628fdd27b6430cca9d
Trym Fanebust <669914@stud.hvl.no>
2024-11-06 18:28:17 +0100

    Oppgave 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -5,11 +5,21 @@ import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        String fullFilsti = mappe + "/" + filnavn;
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+        try (PrintWriter skriver = new PrintWriter(new FileWriter(fullFilsti))) {
+            skriver.print(samling.toString());
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace(); 
+            return false;
+        }
+    }
 }
